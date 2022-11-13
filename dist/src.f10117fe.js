0d39eb010dc6f890f8b0675f1e0ca29c6792842d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2022-11-13 15:30:55 +0100

    create user class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,40 +117,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/models/User.ts":[function(require,module,exports) {
-"use strict";
+})({"src/index.ts":[function(require,module,exports) {
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.User = void 0;
-var User = /** @class */function () {
-  function User(data) {
-    this.data = data;
-  }
-  User.prototype.get = function (propName) {
-    return this.data[propName];
-  };
-  User.prototype.set = function (update) {
-    Object.assign(this.data, update);
-  };
-  return User;
-}();
-exports.User = User;
-},{}],"src/index.ts":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var User_1 = require("./models/User");
-var user = new User_1.User({});
-user.set({
-  name: "andrew"
-});
-console.log(user.get("name"));
-console.log(user.get("age"));
-},{"./models/User":"src/models/User.ts"}],"../../.nvm/versions/node/v12.19.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../.nvm/versions/node/v12.19.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -175,7 +144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62209" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63428" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "./models/User";
-
-const user = new User({});
-
-user.set({ name: "andrew" });
-
-console.log(user.get("name"));
-console.log(user.get("age"));
