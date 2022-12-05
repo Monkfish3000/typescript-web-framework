86a1eda90431ac7975d2edbfad9db8a133dd49c5
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2022-12-05 13:15:45 +0100

    add onRender method to userEdit to render userShow and userForm

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { View } from './View';
 import { User, UserProps } from '../models/User';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 
 export class UserEdit extends View<User, UserProps> {
   regionsMap(): { [key: string]: string } {
@@ -8,6 +10,12 @@ export class UserEdit extends View<User, UserProps> {
       userForm: '.user-form',
     };
   }
+
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
+  }
+
   template(): string {
     return `
         <div>
